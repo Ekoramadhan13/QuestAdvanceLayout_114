2fe049cc5b0c5fb33cfa2394841e5be11cc27afc
Ekoramadhan13 <enugraha411@gmail.com>
2025-10-23 15:55:38 +0700

    Menambahkan teks nama universitas menggunakan stringResource

--- app/src/main/java/com/example/pratikum4/pratikum4.kt
+++ app/src/main/java/com/example/pratikum4/pratikum4.kt
@@ -40,5 +40,9 @@ fun ActivitasPertama(modifier: Modifier){
             text = stringResource(id= R.string.prodi),
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold
-
+        )
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 22.sp
+        )
     }
