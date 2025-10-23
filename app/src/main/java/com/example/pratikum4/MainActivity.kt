14b49d9fcd30792e96effcec71be1aeaec9b6e76
Ekoramadhan13 <enugraha411@gmail.com>
2025-10-23 15:50:09 +0700

    Inisialisasi proyek Compose dengan package com.example.pratikum4

---
+++ app/src/main/java/com/example/pratikum4/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.pratikum4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myuipertama.ActivitasPertama
+import com.example.pratikum4.ui.theme.Pratikum4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pratikum4Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ActivitasPertama(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Pratikum4Theme {
+        Greeting("Android")
+    }
+}
