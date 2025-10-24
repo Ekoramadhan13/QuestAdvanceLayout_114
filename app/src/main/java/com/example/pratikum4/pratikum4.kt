339ee46d5125faa39a19e39d53f1f067f5754a11
Ekoramadhan13 <enugraha411@gmail.com>
2025-10-24 16:20:01 +0700

    Menambahkan teks NIM dengan ukuran 20sp dan warna putih

--- app/src/main/java/com/example/pratikum4/pratikum4.kt
+++ app/src/main/java/com/example/pratikum4/pratikum4.kt
@@ -122,6 +122,12 @@ fun ActivitasPertama(modifier: Modifier) {
                         color = Color.White,
                         modifier = Modifier.padding(top = 15.dp)
                     )
+                    Text (
+                        stringResource(R.string.nim),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
 
                 }
             }
